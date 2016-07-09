7527d359095939b20b48eb053ee641a34db1770b
Tamas Zsebe <tzsebe@gmail.com>
2016-07-09 08:33:36 -0700

    Making user list in lobby clickable, and displaying a modal dialog with the profile info.

--- amoba-live/client/js/helpers.js
+++ amoba-live/client/js/helpers.js
@@ -2,6 +2,10 @@
 // Helpers anyone can use.
 //
 
+Template.registerHelper("isMyUser", function(user) {
+    return (user && Meteor.user() && user._id == Meteor.user()._id);
+});
+
 Template.registerHelper("displayOnlineStatus", function(status) {
     if (status && status.online) {
         return "online";
--- amoba-live/client/js/lobby.js
+++ amoba-live/client/js/lobby.js
@@ -18,3 +18,24 @@ Template.browse_users.helpers({
         return Meteor.users.find({}, {sort: {'profile.username': 1}});
     }
 });
+
+Template.browse_users.events({
+    'click .js-browse-user': function(event) {
+        if (Meteor.user()) {
+            // Set the data context for the modal, which will be the user for
+            // the template we clicked in.
+            Session.set('browse-user', this);
+        }
+    }
+});
+
+/**
+ * This is a reactive pass-through to reactively pass a user object
+ * to the modal content template, thereby allowing us to re-use the user
+ * profile component here.
+ */
+Template.user_popup_content_adapter.helpers({
+    userFromSession: function() {
+        return Session.get('browse-user');
+    }
+});
